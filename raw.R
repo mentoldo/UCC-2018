d6fb6a86a4c008b15b3fbbb8078e651d4c934a01
mentoldo <matias.alfonso@gmail.com>
2018-10-12 21:37:17 -0300

    Clase de asociacion de variables

--- raw.R
+++ raw.R
@@ -91,7 +91,6 @@ cepram <- read.csv2("./data/cuestionario.csv")
 # 
 # write.csv2(cepram, "./data/cuestionario.csv", row.names = FALSE)
 
-
 names(freq1) <- c("nivel_ed", "freq", "freq.rel", "cum.freq", "rel.cum.freq", "pos")
 
 pos <- sum(freq1$freq) - (cumsum(freq1$freq) - freq1$freq / 2)
@@ -110,3 +109,110 @@ freq1 %>%
           axis.text.x=element_text(color='black',size=12)) +
     coord_polar(theta = "y") +
     scale_y_continuous(breaks = pos, labels = label)
+
+
+## Tablas de contingencia
+tipoEnsennanza <- data.frame(id = 1 : 100,
+                             tipo = sample(c("Democrático", "Autoritario"), 100, replace = TRUE),
+                             rendimiento = sample(c("Alto", "Medio", "Bajo"), 100, replace = TRUE))
+
+write.csv2(tipoEnsennanza, file = "./data/tipoEnsennanza.csv", row.names = FALSE)
+
+tipoEnsennanza$rendimiento <- factor(tipoEnsennanza$rendimiento, labels = c("Bajo", "Medio", "Alto"), ordered = TRUE)
+
+
+
+
+mat <- table(tipoEnsennanza$tipo, tipoEnsennanza$rendimiento)
+mat <- addmargins(mat)
+
+
+## Tablas de contingencia 2x2
+tipoEnsennanza <- data.frame(id = 1 : 100,
+                             tipo = sample(c("Democrático", "Autoritario"), 100, replace = TRUE),
+                             rendimiento = sample(c("Alto", "Bajo"), 100, replace = TRUE))
+
+matriz <- matrix(nrow = 3, ncol = 3)
+matriz[3, 4] <- 
+
+matriz[1, ] <- c(5, 35, 50, 90)
+matriz[2, ] <- c(260, 40, 10, 310)
+
+
+### EPH
+eph <- read.csv2("../data/usu_individual_t117.txt")
+
+quantile(eph$PP04B3_ANO, na.rm = TRUE)
+
+df <- eph %>% 
+    filter(!(PP04B3_ANO %in% c(0, 99)), !is.na(PP04B3_ANO), !is.na(PP08D1), PP08D1 != -9)
+
+df
+range(df$PP08D1, na.rm = T)
+
+plot(df$PP04B3_ANO, df$PP08D1)
+
+
+df <- eph %>% 
+    filter(NIVEL_ED %in% 1:6, !is.na(PP08D1), PP08D1 != -9)
+
+
+plot(df$NIVEL_ED, df$PP08D1)
+
+
+
+quantile(df$PP11G_ANO)
+sum(!is.na(df$PP04B3_ANO))
+hist(df$PP04B3_ANO)
+
+df <- eph %>% 
+    filter(!PP04B3_MES %in% c(0, 99), !is.na(PP04B3_MES))
+
+quantile(df$PP04B3_MES)
+sum(!is.na(df$PP04B3_ANO))
+hist(df$PP04B3_MES)
+
+
+### Correlación, experimento de memoria
+
+edad <- round(rnorm(100, 25, 5))
+npalabras <- round(-0.2 * edad + 25+ rnorm(100, sd = 0.5))
+
+df <- data.frame(edad = edad, npalabras = npalabras)
+
+write.csv2(df, "./data/npalabras_edad.csv", row.names = F)
+
+plot(edad, npalabras)
+
+### r de Pearson
+
+x <- rnorm(100, 20, 4)
+y <- x + rnorm(100, 0.5)
+
+df <- data.frame(x = x, y = y)
+write.csv2(df, "./data/rdfuerte.csv", row.names = FALSE)
+
+plot(x, y)
+
+cor(x, y)
+
+## Directa moderada
+x <- rnorm(100, 20, 4)
+y <- x + rnorm(100, 0.5)
+
+df <- data.frame(x = x, y = y)
+write.csv2(df, "./data/rdfuerte.csv", row.names = FALSE)
+
+plot(x, y)
+
+cor(x, y)
+
+
+
+## inversa fuerte
+
+x <- rnorm(100, 20, 4)
+y <- rnorm(100, 20, 4)
+
+df <- data.frame(x = x, y = y)
+write.csv2(df, "./data/rifuerte.csv", row.names = FALSE)
